de0f52194a3720e64c22615b8e86fd30db696998
ghanshyams92 <35167528+ghanshyams92@users.noreply.github.com>
2021-05-03 23:50:08 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,25 @@
 output "public_ip_address" {
   value = "${ibm_compute_vm_instance.devops_commander_vminstance.ipv4_address}"
 }
+
+# get ip_address 
+output "private_ip_address" {
+  value = "${ibm_compute_vm_instance.devops_commander_vminstance.ipv4_address_private}"
+}
+output "instance_id" {
+  description = "ID of the vm instance"
+  value       = "${ibm_compute_vm_instance.devops_commander_vminstance.id}"
+}
+
+# get Public subnet 
+output "public_subnetid" {
+  value = "${ibm_compute_vm_instance.devops_commander_vminstance.public_subnet_id}"
+}
+
+# get private subnet 
+output "private_subnetid" {
+  value = "${ibm_compute_vm_instance.devops_commander_vminstance.private_subnet_id}"
+}
+
+
+  
